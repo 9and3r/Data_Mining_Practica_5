9976d492b0671c831679db26bd0d8cb979336981
aorbegozo005 <aorbegozo005@ikasle.ehu.es>
2014-11-23 13:23:02 +0100

    Por fin zeozer funtzionatzen

--- OneClassClassifier/src/datos/Main.java
+++ OneClassClassifier/src/datos/Main.java
@@ -1,9 +1,28 @@
 package datos;
 
+import weka.classifiers.functions.LibSVM;
+import weka.core.Instances;
+import weka.core.SelectedTag;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		DatuKargatzaile dk = new DatuKargatzaile(args[0]);
+		dk.setKlasearenPosizioa(-1);
+		LibSVM libSVM = new LibSVM();
+		SelectedTag selectedTag = new SelectedTag(LibSVM.SVMTYPE_ONE_CLASS_SVM, LibSVM.TAGS_SVMTYPE);
+		libSVM.setSVMType(selectedTag);
+		try {
+			libSVM.buildClassifier(dk.getInstantziak());
+			Instances instantziak = dk.getInstantziak();
+			for(int i=0;i<instantziak.numInstances();i++){
+				System.out.println(libSVM.classifyInstance(instantziak.instance(i)));
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
