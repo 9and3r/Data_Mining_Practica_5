740c4dd9a82142678a716690fc44434d1be25b9c
Alex Valero <avalero008@ikasle.ehu.es>
2014-11-23 19:34:43 +0100

    SetClassIndex gehituta eta pantailaratutako mezua aldatuta

--- classcounter/src/classcounter/Probaclasscounter.java
+++ classcounter/src/classcounter/Probaclasscounter.java
@@ -7,10 +7,8 @@ public class Probaclasscounter {
 			if(args.length == 2){
 				FitxategiKargatzaile fitxategia = new FitxategiKargatzaile(args[0],Integer.parseInt(args[1]));
 				Classcounter klasekont = new Classcounter();
-				int[] kont=klasekont.contarClases(fitxategia.getInstantziak(), Integer.parseInt(args[1]));
-				for(int i=0;i<kont.length;i++){
-					System.out.println(kont[i]);
-				}
+				klasekont.contarClases(fitxategia.getInstantziak(), Integer.parseInt(args[1]));
+				
 			}
 	}
 }
