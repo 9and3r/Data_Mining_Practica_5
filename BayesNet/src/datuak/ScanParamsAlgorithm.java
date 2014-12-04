4b91decec31c6da48eb3caa1ae904ef3b52a306f
Alex Valero <avalero008@ikasle.ehu.es>
2014-12-04 12:01:45 +0100

    BayesNet jar igo eta parametroen alerta jarri

--- BayesNet/src/datuak/ScanParamsAlgorithm.java
+++ BayesNet/src/datuak/ScanParamsAlgorithm.java
@@ -10,53 +10,54 @@ import weka.core.Instances;
 public class ScanParamsAlgorithm {
 
 	public static void main(String[] args) throws Exception {
-		System.out.println(args.length);
-		if(args.length<3){
-			System.out.println("Bi path sartu behar dira. Lehenengoan entrenatzeko fitxategia, bigarrena klasifikatu behar den fitxategia, azkena klasearen posizioa. Azkena bada -1 sartu");
+		/***************************************
+		 Argumentos
+			arg[0] = Path fichero train
+			args[1] = Posicion de la clase (En caso de indicar -1 sera el ultimo)
+			args[2] = Path fichero dev
+			args[3] = Path donde se creara el cls
+		 ****************************************/
+		if(args.length<4){
+			System.out.println("Beharrezko parametroak:");
+			System.out.println("   arg[0] = Path fichero train");
+			System.out.println("   args[1] = Posicion de la clase (En caso de indicar -1 sera el ultimo)");
+			System.out.println("   args[2] = Path fichero dev"); 
+			System.out.println("   args[3] = Path donde se creara el modelo del clasificador");		
 		}else{
 			//CASO 1
 			if(args.length==4){
-			FitxategiKargatzaile f=new FitxategiKargatzaile(args[0], Integer.parseInt(args[1]));
-			FitxategiKargatzaile fdev=new FitxategiKargatzaile(args[2], Integer.parseInt(args[1]));
-			Instances instantziak = f.getInstantziak();
-			Instances dev = fdev.getInstantziak();
-			BayesNetProba oraingoa=null;
-			BayesNetProba altuena=null;			
-			//klase minoritarioa lortzeko kodea, klase honen fmeasure-a optimizatu nahi dugu
-
-			Classcounter classcounter = new Classcounter();
-			int[] emaitza = classcounter.contarClases(instantziak,	Integer.parseInt(args[1]) );
-			int klaseMinoritarioa=0;
-			for (int i=1; i<emaitza.length; i++){
-				if(emaitza[i]<emaitza[klaseMinoritarioa])
-					klaseMinoritarioa=i;
-			}
-			////////////////////////////////////
-			
-			for(int i=0;i<2;i++){
-				if(i==0){
-					altuena=konprobatuAtributuGuztiak(instantziak, true, dev, klaseMinoritarioa);
-				}else{
-					oraingoa=konprobatuAtributuGuztiak(instantziak, false, dev,klaseMinoritarioa);
+				FitxategiKargatzaile f=new FitxategiKargatzaile(args[0], Integer.parseInt(args[1]));
+				FitxategiKargatzaile fdev=new FitxategiKargatzaile(args[2], Integer.parseInt(args[1]));
+				Instances instantziak = f.getInstantziak();
+				Instances dev = fdev.getInstantziak();
+				BayesNetProba oraingoa=null;
+				BayesNetProba altuena=null;			
+				//klase minoritarioa lortzeko kodea, klase honen fmeasure-a optimizatu nahi dugu
+				Classcounter classcounter = new Classcounter();
+				int[] emaitza = classcounter.contarClases(instantziak,	Integer.parseInt(args[1]) );
+				int klaseMinoritarioa=0;
+				for (int i=1; i<emaitza.length; i++){
+					if(emaitza[i]<emaitza[klaseMinoritarioa])
+						klaseMinoritarioa=i;
 				}
-			}	
-			if(oraingoa.getFmeasure()>altuena.getFmeasure()){
-				altuena=oraingoa;
-			}
-			altuena.imprimatuEzZintzoa();
-			altuena.imprimatuZintzoa();
-			FitxategiKargatzaile f2=new FitxategiKargatzaile(args[2], Integer.valueOf(args[1]));
-			altuena.erabakiakHartu(f2.getInstantziak()); 
-			altuena.sailkatzaileaGorde(args[3]);
-			
-			/*
-			//Importatu sortutako modeloa eta proba egin (probisionala)
-			Classifier cls = (Classifier) weka.core.SerializationHelper.read(args[3]);
-			Evaluation evaluator = new Evaluation(instantziak);
-			evaluator.evaluateModel(cls, fdev.getInstantziak());
-			System.out.println(evaluator.fMeasure(klaseMinoritarioa));
-			*/}
-			
+				////////////////////////////////////
+				
+				for(int i=0;i<2;i++){
+					if(i==0){
+						altuena=konprobatuAtributuGuztiak(instantziak, true, dev, klaseMinoritarioa);
+					}else{
+						oraingoa=konprobatuAtributuGuztiak(instantziak, false, dev,klaseMinoritarioa);
+					}
+				}	
+				if(oraingoa.getFmeasure()>altuena.getFmeasure()){
+					altuena=oraingoa;
+				}
+				//altuena.imprimatuEzZintzoa();
+				altuena.imprimatuZintzoa();
+				FitxategiKargatzaile f2=new FitxategiKargatzaile(args[2], Integer.valueOf(args[1]));
+				altuena.erabakiakHartu(f2.getInstantziak()); 
+				altuena.sailkatzaileaGorde(args[3]);
+			}		
 			}
 	}
 	
